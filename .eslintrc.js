b8624b18ce82073688e9d42ea15fe513ac8c34d3
Dmitry Manannikov <slonoed@uber.com>
2019-02-06 18:36:20 +0100

    Add lint

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  extends: 'eslint:recommended',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+    indent: ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'never'],
+    'react/jsx-uses-vars': [2],
+    'react/jsx-uses-react': [2],
+  },
+  globals: {
+    it: true,
+  },
+}
