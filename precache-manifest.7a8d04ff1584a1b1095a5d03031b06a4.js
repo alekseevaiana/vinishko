36e59b9ec27f369ae47cde611b90c73ac01b2701
alekseevayana <alekseevayana@bk.ru>
2019-03-19 20:43:09 +0100

    Updates

---
+++ precache-manifest.7a8d04ff1584a1b1095a5d03031b06a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "23ca37b489335b41035cb4e7ee633950",
+    "url": "/vinishko/static/media/arrow-back.23ca37b4.svg"
+  },
+  {
+    "revision": "104d325408621e72a33b",
+    "url": "/vinishko/static/js/runtime~main.104d3254.js"
+  },
+  {
+    "revision": "2f5e7c0131651abb2d9c",
+    "url": "/vinishko/static/js/main.2f5e7c01.chunk.js"
+  },
+  {
+    "revision": "9287393d8915055d06dd",
+    "url": "/vinishko/static/js/1.9287393d.chunk.js"
+  },
+  {
+    "revision": "2f5e7c0131651abb2d9c",
+    "url": "/vinishko/static/css/main.9dbc9b48.chunk.css"
+  },
+  {
+    "revision": "b86bbba843100c71e9160bdd86f163ad",
+    "url": "/vinishko/index.html"
+  }
+];
